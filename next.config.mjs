826f5f2b3f852a30398dc37ecf7df33a06e3662d
FadnisW <fadnisw@gmail.com>
2025-08-28 18:26:35 +0530

    Added the parameters for 400 error

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:['localhost', '127.0.0.1']
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'http',
+                hostname: '127.0.0.1',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'grocery-backend-rhiu.onrender.com',
+                port: '',
+                pathname: '/uploads/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'grocery-frontend-n72vixfbo-waqars-projects-1ec04b0c.vercel.app',
+                port: '',
+                pathname: '/**',
+            }
+        ]
     },
     reactStrictMode:false
 };
